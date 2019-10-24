ed2eef5e4c9394025053b4b36c380bf8300fb344
Adam Epling <adamepling@gmail.com>
2019-10-24 15:48:23 -0500

    Add prettier (#1)
    * added prettier
    * add js to prettier-checking

--- amqp_publisher.js
+++ amqp_publisher.js
@@ -2,21 +2,21 @@ const QUEUE = process.env.QUEUE_NAME;
 const ADDR = process.env.RABBIT_MQ_ADDR;
 
 async function amqp_publisher() {
-    try {
-        let con = await require('amqplib').connect(ADDR);
-        let ch = await con.createChannel();
-        await ch.assertQueue(QUEUE, { durable: true });
+  try {
+    let con = await require('amqplib').connect(ADDR);
+    let ch = await con.createChannel();
+    await ch.assertQueue(QUEUE, { durable: true });
 
-        let sendToAMQP = messgString => {
-            ch.sendToQueue(QUEUE, Buffer.from(messgString));
-        };
+    let sendToAMQP = messgString => {
+      ch.sendToQueue(QUEUE, Buffer.from(messgString));
+    };
 
-        return sendToAMQP;
-    } catch (error) {
-        console.log('ERR:' + error);
-    }
+    return sendToAMQP;
+  } catch (error) {
+    console.log('ERR:' + error);
+  }
 }
 
 module.exports = {
-    amqp_publisher
+  amqp_publisher
 };
--- main.js
+++ main.js
@@ -9,66 +9,52 @@ app.use(express.static('client'));
 
 let publisher;
 let clients = [];
-let consumerTag;
-let consumerChannel;
 
-async function consumeMsgs() {
-    console.log(`[✔] Starting consumer...`);
+async function init() {
+  try {
+    publisher = await amqp_publisher();
+    console.log(`[*] Starting consumer...`);
     let { amqpResponses } = await amqp_consumer();
-    let info = await amqpResponses(rspAMQP => {
-        let msg = JSON.parse(rspAMQP);
-        clients.forEach(client => {
-            let clientId = client.client.conn.id;
-            if (clientId !== msg.senderId) {
-                client.emit('msg_from_server', msg);
-            }
-        });
+    await amqpResponses(rspAMQP => {
+      let msg = JSON.parse(rspAMQP);
+      clients.forEach(client => {
+        let clientId = client.client.conn.id;
+        if (clientId !== msg.senderId) {
+          client.emit('msg_from_server', msg);
+        }
+      });
     });
-    consumerTag = info.consumerTag;
-    consumerChannel = info.channel;
-}
+    console.log('[✔] rabbitMQ ready');
 
-async function WSConnect() {
-    try {
-        publisher = await amqp_publisher();
-        console.log('[*] rabbitMQ ready');
+    io.on('connection', function(socket) {
+      let id = socket.client.conn.id;
+      clients.push(socket);
+      console.log(`[+] Connected to Client ID: ${id}`);
 
-        io.on('connection', function(socket) {
-            let id = socket.client.conn.id;
-            clients.push(socket);
-            console.log(`[+] Connected to Client ID: ${id}`);
-            if (clients.length === 2) {
-                consumeMsgs();
-            }
+      socket.on('msg_to_server', str => {
+        if (!str.senderId) {
+          str.senderId = id;
+        }
+        publisher(JSON.stringify(str));
+      });
 
-            socket.on('msg_to_server', str => {
-                if (!str.senderId) {
-                    str.senderId = id;
-                }
-                publisher(JSON.stringify(str));
-            });
-
-            socket.on('disconnect', arg => {
-                console.log(`[-] Disconnected from Client ID: ${id}`);
-                let i = clients.findIndex(c => c.client.conn.id === id);
-                if (i !== -1) {
-                    clients.splice(i, 1);
-                    if (clients.length === 1) {
-                        console.log(`[×] No clients left to consume messages. Stopping consumer...`);
-                        consumerChannel.cancel(consumerTag);
-                    }
-                }
-            });
-        });
-    } catch (err) {
-        console.log('ERR: ', err);
-    }
+      socket.on('disconnect', arg => {
+        console.log(`[-] Disconnected from Client ID: ${id}`);
+        let i = clients.findIndex(c => c.client.conn.id === id);
+        if (i !== -1) {
+          clients.splice(i, 1);
+        }
+      });
+    });
+  } catch (err) {
+    console.log('ERR: ', err);
+  }
 }
 
 let server = http.createServer(app);
 let io = require('socket.io')(server);
 
 server.listen(3000, () => {
-    console.log(`[*] Server is listening on port 3000`);
-    WSConnect();
+  console.log(`[*] Server is listening on port 3000`);
+  init();
 });
